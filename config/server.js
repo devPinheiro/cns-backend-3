ead9a0f3140d74146e1cd95001d45188ff909f17
devPinheiro <pinheirolaoluwa@gmail.com>
2021-01-14 11:39:53 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eaeb4d59297bf1385b3a539668a4bb37'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2eb6de42-9107-4999-beb2-294403a45943'
+};
